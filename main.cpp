e72dd5acc7fb77ae2ba2a9e5f521d0b714ca64de
SunJun <xiaopisung@foxmail.com>
2021-10-24 19:44:33 +0800

    include reference_learn.h

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "tuple_learn.h"
 #include "loop_learn.h"
 #include "class_learn.h"
+#include "reference_learn.h"
 
 extern void write();  //行后的注释要空两格
 
@@ -15,9 +16,17 @@ int main() {
 //    loop_learn();
 
     //测试类的调用
-    MyBox my_box;
-    my_box.Set(11,2.1,3.3);
-    std::cout << "正方体的体积是：" << my_box.GetVolume() << std::endl;
+//    MyBox my_box;
+//    my_box.Set(11,2.1,3.3);
+//    std::cout << "正方体的体积是：" << my_box.GetVolume() << std::endl;
+
+    //测试引用
+    int var{1};
+    ReferenceLearn reference_learn;
+    reference_learn.SetMyNumber(var);
+    reference_learn.GetMyNumber();
+
+
 
     return 0;
 }
